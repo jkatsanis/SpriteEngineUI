32e8fce8289bcf754a5fedeca827812aa3ce0405
Paul Schein <p.schein@students.htl-leonding.ac.at>
2024-06-26 13:02:00 +0200

    added submodules

--- Engine/Core/Source/Include/ImGui/ImGUISFML.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "imgui.h"
-#include "imgui-SFML.h"
-
-//https://oprypin.github.io/crystal-imgui/ImGui/ImGuiKey.html -> keyboard keys
---
+++ Engine/ThirdParty/UtilityFunctions.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <filesystem>
+#include <Windows.h>
+
+#include "Core/EngineData.h"
+#include "Core/Log.h"
+
+namespace spe
+{
+	class Utility
+	{
+	public:
+		Utility() = delete;
+
+		static std::string BoolToStr(bool b);
+		static void CreateFileWithContent(const std::string& content, const std::string& pathAndName);
+
+		static bool Contains(const std::string& str, const std::vector<std::string>& arr);
+		static std::string VectorToString(const std::vector<std::string>& vec, char del, const std::string& ext);
+		static std::string RenamePartOnPath(const std::string& path, const std::string& new_name, char del, const std::string& ext, uint32_t pos);
+		static void SetCurrentDir(const std::string& path);
+		static std::string getUserProjectPathSeperatetFromEnginePath(const std::string& path);
+		static std::vector<std::string> Split(const std::string& s, char delim);
+		static void WriteFile(const std::string& name, const std::string& path);
+
+		static std::string GetFileExtension(const std::string& file);
+		static std::string GetNamePathSplit(std::string path);
+		static void Delete(const std::string& path);
+
+		static std::string RunCommand(const char* commad);
+
+		/// <summary>
+		/// Should only be used 1 time. 
+		/// </summary>
+		static std::string GetDefaultDir(uint32_t depth);
+
+		static std::string GetCurrentDir();
+
+		static bool IsStringValid(const std::string& path);
+
+		static bool IsFolder(const std::string& path);
+
+		static std::string RemoveExtension(const std::string& file);
+
+		static std::string CopyDir(const std::string& inputDir, const std::string& outputdir, const std::string& name);
+
+		static void GetFilePathWithExtensionInFolder(const std::filesystem::path& path, const std::string& extension, std::vector<std::string>& to);
+	};
+}
